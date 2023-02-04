a041114a74dbe3b04be9d1035d6c8bf8ea710e0f
gwonmin <didrnjsals@gmail.com>
2023-02-04 17:37:17 +0900

    feat: 프로젝트 기본 세팅 완료

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,10 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <MainTitle title="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+<script setup lang="ts">
+import MainTitle from '@/components/MainTitle.vue'; // @ is an alias to /src
 </script>
