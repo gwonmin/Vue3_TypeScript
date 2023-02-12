82f2989ef94cfd65d3d2ec218b9cd4e5b2c9bcb4
[4기-1반] 양권민 <didrnjsals@gmail.com>
2023-02-12 18:21:00 +0900

    feat: el-container 컴포넌트 개발완료

---
+++ src/components/HeaderMainFooterLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-container>
+    <el-header>
+      <slot name="header"></slot>
+    </el-header>
+    <el-main>
+      <slot name="main"></slot>
+    </el-main>
+    <el-footer>
+      <slot name="footer"></slot>
+    </el-footer>
+  </el-container>
+</template>
